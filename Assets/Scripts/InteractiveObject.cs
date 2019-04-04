acf497d015c94ba97ff8e298532f3631ab424197
Quarot <corneljackson1@gmail.com>
2019-04-04 14:35:38 -0500

    Implemented Sound Effects, and started on toggle system

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class InteractiveObject : MonoBehaviour, IInteractive
 {
     [SerializeField]
-    private string displayText;
+    private string displayText = nameof(InteractiveObject);
+
+    [SerializeField]
+    private string responseText;
 
     public string DisplayText => displayText;
+    private AudioSource audioSource;
 
-    public void InteractWith()
+    private void Awake()
     {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public virtual void InteractWith()
+    {
+        try
+        {
+            audioSource.Play(); 
+
+        }
+        catch (System.Exception)
+        {
+
+            throw new System.Exception("Missing audiosource component: InteractiveObject requires an audioSource component.");
+        }
         Debug.Log($"Player just interacted with {gameObject.name}.");
     }
 }
---
+++ Assets/Scripts/ToggleSetActive.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleSetActive : InteractiveObject
+{
+    [Tooltip("The Game Object to Toggle")]
+    [SerializeField]
+    private GameObject objectToToggle;
+
+    /// <summary>
+    /// Toggles the activeSelf value for the objectToToggle when the player interacts with this item.
+    /// </summary>
+    
+    public override void InteractWith()
+    {
+        base.InteractWith();
+        objectToToggle.SetActive(!objectToToggle.activeSelf);
+    }
+}
